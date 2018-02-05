f9acab63594c07762ffd87e68ef47bf5b0e7bc5c
Jan Biedermann <jan@kursator.de>
2018-02-05 08:55:12 +0100

    add default_branch

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -72,6 +72,6 @@ class ProjectsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def project_params
-      params.require(:project).permit(:repo, :github_host)
+      params.require(:project).permit(:repo, :github_host, :default_branch)
     end
 end
