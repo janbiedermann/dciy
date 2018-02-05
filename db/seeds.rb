79702cc77f66448e8b1e1531e795adea612d71aa
Jan Biedermann <jan@kursator.de>
2018-02-05 16:01:21 +0100

    seed for hyperloop repos

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-component.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-mesh.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-model.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-operation.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-react.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-router.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-spec.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyper-store.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyperloop.git', default_branch: '1_0_0')
+Project.create(github_host: 'github.com', repo: 'ruby-hyperloop/hyperloop-config.git', default_branch: '1_0_0')
